9358d47161d72f83d597c69f731ad4cf75b36d81
mohd-maruf-kureshi-2828 <mohamedmarufkureshikureshi@gmail.com>
2025-04-02 00:13:00 +0530

    add javascript file

--- iteration_05/02_while_do.js
+++ iteration_05/02_while_do.js
@@ -1,8 +1,44 @@
-let i=0;
-while(i<=10){
-    // console.log(i);
-    i++
-}
+// let i=0;
+// while(i<=10){
+//     // console.log(i);
+//     i++
+// }
 
 
+//while loop table 
+// let i = 1 //starting iteration
+// while (i <= 10)  //condition {
+//     console.log(`5 X ${i} = ${5 * i}`)
+//     i++;  //update if the condition is true
+// }
+
+
+// this code open in webbrowser because here we use prompt function
+// let userNum;
+// let positive;
+// while (isNaN(userNum) || positive < 0) {
+//     userNum = prompt("Enter Yout Positive Number ")
+//     positive = parseFloat(userNum)
+
+// }
+// console.log(`your entered a valid positive number ${positive}`)
+
+
+//>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>><<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+//table do while loop
+// let i=1 //starting iteration
+// do{
+//     console.log(`5 X ${i} =${5*i}`);
+//     i++; //update if the condition is true
+    
+// }while(i<=10) //condition
+
+// let userNum;
+// let positive;
+// do {
+//     userNum = prompt("Enter Yout Positive Number ")
+//     positive = parseFloat(userNum)
+// } while (isNaN(userNum) || positive < 0);
+// console.log(`your entered a valid positive number ${positive}`)
 
