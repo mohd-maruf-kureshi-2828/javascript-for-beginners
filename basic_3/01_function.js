783bf686015348ccaaaf85336a1bbb5dcd6fc4c9
mohd-maruf-kureshi-2828 <mohamedmarufkureshikureshi@gmail.com>
2025-07-01 18:23:05 +0530

    update basic 3

--- basic_3/01_function.js
+++ basic_3/01_function.js
@@ -46,23 +46,46 @@ function loginUser(username){
 }
 // console.log(loginUser());
 
-// function myNums(n1,n2,...n3){
-//      return n3
-// }
-// console.log(myNums(100,222,222,22233,222,234,1009));
+
+function myNums(n1, n2, ...n3) {
+    return [n1, n2, ...n3];
+}
+// console.log(myNums(100, 222, 222, 22233, 222, 234, 1009));
 
 
 let myobj={
     user:"maruf",
     email:"maruf@gmail.com"
 }
-// function accesobj(anyobject){
-//     return `user name is ${anyobject.user} and user gmail is ${anyobject.email}`
-// }
+function accesobj(anyobject){
+    return `user name is ${anyobject.user} and user gmail is ${anyobject.email}`
+}
 // console.log(accesobj(myobj));
 
+
 //second tarika
-// myobj.access=function(){
-//     return`your user name is ${this.user}`
-// }
+myobj.access=function(){
+    return`your user name is ${this.user}`
+}
 // console.log(myobj.access());
+
+
+//direct object bhi rikh sakte hai
+let directAcc=accesobj({
+    user:"kureshi",
+    email:"amazon@gmail.com"
+})
+
+// console.log(directAcc);
+
+
+//array ko bhi access kar sakte hai using funtion
+let myArray=[1,2,,3,4,5,6]
+function anyArray(array1){
+    console.log(`Your Array Is ${array1}`)
+    console.log(`First Index OF Your Array Is ${array1[0]}`)
+}
+// anyArray(myArray)
+
+//direct Array bhi rikh sakte hai
+anyArray([500,200,3000])
