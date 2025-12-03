8f8763411583d338fc6225484adef89b17af8812
Mohamed maruf kureshi <mohamedmarufkureshikureshi@gmail.com>
2025-12-03 13:28:34 +0000

    BackGroundColor_Changer

---
+++ dom_basic_project/color_changer/script.js
@@ -0,0 +1,20 @@
+const btn = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+
+btn.forEach((button) => {
+    // console.log(button)
+    button.addEventListener('click', (e) => {
+        // console.log(e)
+        // console.log(e.target.id)
+        if (e.target.id === 'grey') {
+            body.style.backgroundColor = e.target.id
+        } else if (e.target.id === 'white') {
+            body.style.backgroundColor = e.target.id
+        } else if (e.target.id === 'blue') {
+            body.style.backgroundColor = e.target.id
+        } else if (e.target.id === 'yellow') {
+            body.style.backgroundColor = e.target.id
+        }
+    })
+
+})
